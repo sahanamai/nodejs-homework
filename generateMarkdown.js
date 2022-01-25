45d51655fa01b78c2129c10547d831f6664840c6
sahana <sahanamai@gmail.com>
2022-01-24 18:35:23 -0600

    samplereadme updated with license

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,6 @@
 function renderLicenseBadge(license) {
     if (license==='MIT') {
-      return '[![License: MIT](https://img.shields.io/badge/${license}-yellow.svg)](https://opensource.org/licenses/MIT)'
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
     } 
     else if (license==='Apache 2.0') {
     return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
@@ -13,30 +13,32 @@ function renderLicenseBadge(license) {
 }
    
 function generateMarkdown(data) {
+    let badge = renderLicenseBadge(data.license)
     return `# ${data.project_name}
-    ## Description 
-    ${data.description}
-   ## Table of contents
-     * [Installation](#installation)
-     * [Usage](#usage)
-     * [Test](#test)
-     * [Contribution](#contribution)
-     * [License](#license)
-     * [Questions](#questions)
+## Description 
+ ${data.description}
+## Badge
+* ${badge}
+## Table of contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Test](#test)
+  * [Contribution](#contribution)
+  * [License](#license)
+  * [Questions](#questions)
      
-    ## Installation
-    The following dependencies must be installed to run the application ${data.install}.
-    ## Usage
-    ${data.usage}
-    ## License
-    ${renderLicenseBadge(data.license)}
-    This project is licensed under the ${data.license} license.
-    ## Test
-    ${data.test}
-    ## Contribution
-    Contributors:${data.contribution}
-    ## Questions
-    If you have any questions about the repo, open an issue or contact ${data.name} directly at: ${data.email}
+ ## Installation
+ The following dependencies must be installed to run the application ${data.install}.
+ ## Usage
+  ${data.usage}
+ ## License
+  This project is licensed under the ${data.license} license.
+ ## Test
+  ${data.test}
+ ## Contribution
+  Contributors:${data.contribution}
+ ## Questions
+  If you have any questions about the repo, open an issue or contact ${data.name} directly at: ${data.email}
     
 
     `;
--- samplereadme.md
+++ samplereadme.md
@@ -1,27 +1,28 @@
-# README generator
- ## Description 
-    This app allows developer to create a professional readme.md file in a way that is clean,efficient and easy to use.
+# README Generator
+## Description 
+ This app allows developer to create a professional Readme.md file in a way that is clean,efficient and easy to use.
+## Badge
+* [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 ## Table of contents
-* [Installation](#installation)
-* [Usage](#usage)
-* [Test](#test)
-* [Contribution](#contribution)
-* [License](#license)
-* [Questions](#questions)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Test](#test)
+  * [Contribution](#contribution)
+  * [License](#license)
+  * [Questions](#questions)
      
-## Installation
-    The following dependencies must be installed to run the application inquire,fs.
+ ## Installation
+ The following dependencies must be installed to run the application inquire,fs.
  ## Usage
-    Its awesome
-## License
-    [![License: MIT](https://img.shields.io/badge/${license}-yellow.svg)](https://opensource.org/licenses/MIT)
-    This project is licensed under the MIT license.
-## Test
-    node index.js
-## Contribution
-    Contributors:N/A
+  Its awesome
+ ## License
+  This project is licensed under the MIT license.
+ ## Test
+  node index.js
+ ## Contribution
+  Contributors:N/A
  ## Questions
-    If you have any questions about the repo, open an issue or contact sahanamai directly at: sahanamai@gmail.com
+  If you have any questions about the repo, open an issue or contact sahanamai directly at: sahanamai@gmail.com
     
 
     
