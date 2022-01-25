179a02a4d126be50d9fd986dd735c6920b56ed59
sahana <sahanamai@gmail.com>
2022-01-24 18:15:19 -0600

    license added

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,6 @@
 function renderLicenseBadge(license) {
     if (license==='MIT') {
-      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+      return '[![License: MIT](https://img.shields.io/badge/${license}-yellow.svg)](https://opensource.org/licenses/MIT)'
     } 
     else if (license==='Apache 2.0') {
     return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
